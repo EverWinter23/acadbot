7090835f1a709a4f9c97c95bcdc70f8a7c1a051b
Ever Winter <eternal.blizzard23@gmail.com>
2018-04-16 03:03:59 +0530

    version.py: Add version command

---
+++ cmds/cmd_version.py
@@ -0,0 +1,17 @@
+'''
+16th april 2018 monday
+simple command file for displaying version
+'''
+VERSION = 'v0.2'
+ACADBOT = 'acadbot'
+
+DEV = 'Devesh Kashmiri'
+VER = 'Rishabh Mehta'
+
+def cmd_version():
+    print(ACADBOT + " " + VERSION)
+    print('')
+
+    print('@authors')
+    print(DEV)
+    print(VER)
