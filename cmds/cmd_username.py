6825143e7ed91892dd02c4f175fb6e536d2a063d
Ever Winter <eternal.blizzard23@gmail.com>
2018-04-16 10:38:18 +0530

    cmd_username.py: Display prev username if config exists

--- cmds/cmd_username.py
+++ cmds/cmd_username.py
@@ -7,10 +7,16 @@ CONFIG_FILE = 'config.txt'
 def cmd_username(name):
     file = Path(CONFIG_FILE)
     if file.exists():
+        with open(file, 'r') as config_file:
+            line = config_file.readline()
+            print('acadbot: Config file already exists for user', line.strip('username:'))
+
         print('acadbot: Do you want to overwrite the existing config?')
         response = input('acadbot: [yes/no]> ')
         if response == 'no':
+            print("acadbot: Leaving config file as is...")
             exit()
+            
         print('acadbot: Existing config will be overwritten...')
     print('acadbot: Writing to config file...')
 
