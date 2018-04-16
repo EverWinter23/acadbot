e9ff4f47174fab82973d6a2aa80cceeb8e127b94
Ever Winter <eternal.blizzard23@gmail.com>
2018-04-16 21:11:27 +0530

    acadbot.py: Add fetch commands

--- acadbot.py
+++ acadbot.py
@@ -19,6 +19,9 @@ COMMANDS = [CONFIG, FETCH, VERSION, HELP]
 CONFIG_ARGS = [USERNAME, PASSWORD]
 FETCH_ARGS = [ATTENDANCE, TIME_TABLE]
 
+# print help when required
+helper = Helper()
+
 def validate_arg(arg):
     if arg not in COMMANDS:
         print("acadbot: '" + arg + "' Command not found... ")
@@ -29,52 +32,94 @@ def validate_arg(arg):
 
 def validate_config_arg(arg):
     if arg not in CONFIG_ARGS: 
-        print("acadbot: config: '" + str(arg) +"' option not found...")
+        print("acadbot: '" + str(arg) +"' option not found...")
         Helper().help_config()
         print('acadbot: Exiting...')
         # error
+        exit()
+
+def validate_fetch_arg(arg):
+    if arg not in FETCH_ARGS:
+        print("acadbot: '" + str(arg) +"' option not found...")
+        Helper().help_fetch()
+        print('acadbot: Exiting...')
         exit()
 
 def main(args):
     parser = ArgParser(args)
 
     validate_arg(parser.cur_arg)
-    # usage: acadbot config --user '151301'
-    # usage: acadbot config --passwd<enter>
     if parser.cur_arg == CONFIG:
-        # config can have the following two params only
+        # config can have the following two args only
         #   1. --user 
         #   2. --passwd
         parser.get_next_arg()
+
+        # if no argument print usage help
         validate_config_arg(parser.cur_arg)
         
+        # usage: acadbot config --user '151301'
         if parser.cur_arg == USERNAME:
             parser.get_next_arg()
             if parser.cur_arg  is not None and parser.cur_arg.isnumeric():
                 cmd_username(parser.cur_arg)
             else:
-                print("acadbot: config: user: '" + str(parser.cur_arg) +"' is an invalid username...")
-                Helper().help_username()
+                print("acadbot: '" + str(parser.cur_arg) +"' is an invalid username...")
+                helper.help_username()
                 print('acadbot: Exiting...')
                 exit()
-                   
+        
+        # usage: acadbot config --passwd<enter>    
         elif parser.cur_arg == PASSWORD:
             parser.get_next_arg()
             if parser.cur_arg is None:                
                 cmd_passwd()
             else:
-                print("acadbot: config: passwd: 'Do not enter password as an argument'")
-                Helper().help_password()
+                print("acadbot: 'Do not enter password as an argument'")
+                helper.help_password()
                 print('acadbot: Exiting...')
                 # error
                 exit()
-        else:
-            # raise error
-            exit()
-
-    # usage: acadbot fetch [attendace | time-table] 
+ 
+    # usage: acadbot fetch [attendance | time-table] 
     elif parser.cur_arg == FETCH:
+        # fetch can have the following two args only
+        #   1. attendance
+        #   2. time-table
+        parser.get_next_arg()
         raise NotImplementedError
+        # if no argument print usage help
+        validate_fetch_arg(parser.cur_arg)
+
+        # usage: acadbot fetch time_table batch [day]
+        if parser.cur_arg == TIME_TABLE:
+            parser.get_next_arg()
+            batch = parser.cur_arg
+            if batch is not None and batch and len(batch) == 2 and batch[0].isaplha() and batch[0].isnumeric():
+                # if day has been passed as an arg
+                parser.get_next_arg()
+                if parser.cur_arg is not None and parser.cur_arg in days:
+                    # TODO here
+                    cmd_time_table(batch, parser.cur_arg)
+
+                elif parser.cur_arg is None:
+                    # TODO here
+                    cmd_time_table(batch)
+                else:
+                    # usage
+                    exit()                
+            else:
+                print("acadbot: '" + str(parser.cur_arg) +"' is an invalid option...")
+                # TODO here
+                helper.help_time_table()
+                print('acadbot: Exiting...')
+                exit()
+        
+        # usage: acadbot fetch attendance
+        elif parser.cur_arg == ATTENDANCE:
+            parser.get_next_arg()
+            # TODO here
+            cmd_attendance()        
 
     # usage: acadbot --version
     elif parser.cur_arg == VERSION:
@@ -84,28 +129,32 @@ def main(args):
     elif parser.cur_arg == HELP:
         parser.get_next_arg()
         if parser.cur_arg == CONFIG:
-            Helper().help_config()
+            helper.help_config()
+
         elif parser.cur_arg == FETCH:
-            Helper().help_fetch()
+            helper.help_fetch()
+
         elif parser.cur_arg == HELP:
-            Helper().help_helper()
-            Helper().help_lists()
+            helper.help_helper()
+            helper.help_lists()
+
         elif parser.cur_arg == ALL:
-            Helper().help_command()
-        elif parser.cur_arg == 'version':
-            Helper().help_version()
+            helper.help_command()
+
+        elif parser.cur_arg == VERSION:
+            helper.help_version()
+
         else:
-            Helper().help_helper()
-            Helper().help_lists()
+            helper.help_helper()
+            helper.help_lists()
           
 
 
 if __name__ == "__main__":
     user_args = sys.argv[1:]
     if len(user_args) == 0:
-        #cmd_version()
-        #help_usage()
-        # also print usage message
+        helper.help_helper()
+        helper.help_lists()
         exit()
 
     main(user_args)
