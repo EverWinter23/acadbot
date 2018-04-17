d9e41829f311b467ff418e492ec60c908c534877
Ever Winter <eternal.blizzard23@gmail.com>
2018-04-17 22:32:03 +0530

    links.py: File containing all links for url requests

---
+++ lib/links.py
@@ -0,0 +1,20 @@
+'''
+17th april 2018 tuesday
+'''
+
+# somehow must hide the api key
+KEY = 'DtH4yJJM24a0QzCHuVjPl7tWTsYKsVOc89vClUmE'
+
+# important urls
+URL_HOST = "12s51emmid.execute-api.ap-south-1.amazonaws.com"
+
+URL_BASE = "https://" + URL_HOST + "/v1/"                       # works
+URL_LOGIN = "JUITKiosk_Login"                                   # works
+URL_USER_DETAILS = "JUITKiosk_UserDetails"                      # works
+URL_ATTENDANCE = "JUITKiosk_Attendance"                         # works
+URL_ATTENDANCE_DETAIL = "JUITKiosk_DetailAttendance"            # works
+URL_SUBJECT_REGISTD = "JUITKiosk_Academic_RegisteredSubjects"   # works
+URL_SUBJECT_FACULTY = "JUITKiosk_Academics_SubjectFaculty"      # works
+URL_SGPA_CGPA = "JUITKiosk_ExamInfo_SGPA_CGPA"                  # works
+URL_SEM_LIST = "JUITKiosk_ExamInfo_GetSemesters"                # works
+URL_EXAM_GRADES = "JUITKiosk_ExamInfo_GetGradesBySem"           # works
