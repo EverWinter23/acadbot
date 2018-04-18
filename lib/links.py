fec563ac6a2ddfc79721c4f1c79069fdba5ac288
Ever Winter <eternal.blizzard23@gmail.com>
2018-04-18 22:07:43 +0530

    links.py: Add acadbot server links

--- lib/links.py
+++ lib/links.py
@@ -2,12 +2,8 @@
 17th april 2018 tuesday
 '''
 
-# somehow must hide the api key
-KEY = 'DtH4yJJM24a0QzCHuVjPl7tWTsYKsVOc89vClUmE'
-
 # important urls
 URL_HOST = "12s51emmid.execute-api.ap-south-1.amazonaws.com"
-
 URL_BASE = "https://" + URL_HOST + "/v1/"                       # works
 URL_LOGIN = "JUITKiosk_Login"                                   # works
 URL_USER_DETAILS = "JUITKiosk_UserDetails"                      # works
@@ -18,3 +14,7 @@ URL_SUBJECT_FACULTY = "JUITKiosk_Academics_SubjectFaculty"      # works
 URL_SGPA_CGPA = "JUITKiosk_ExamInfo_SGPA_CGPA"                  # works
 URL_SEM_LIST = "JUITKiosk_ExamInfo_GetSemesters"                # works
 URL_EXAM_GRADES = "JUITKiosk_ExamInfo_GetGradesBySem"           # works
+
+# heroku acadbot server links
+HEROKU_ACADBOT_SERVER_URL = 'https://acadbot23.herokuapp.com/'
+KEY = 'X-Api-Key'
