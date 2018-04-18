04710e3d4bd138f470533ed45d515e17f5e9fe08
Ever Winter <eternal.blizzard23@gmail.com>
2018-04-18 22:30:13 +0530

    setup.py: Setup for acadbot, creates bin file

---
+++ setup.py
@@ -0,0 +1,14 @@
+'''
+18th april 2018 wednesday
+'''
+from distutils.core import setup
+setup(name='acadbot',
+           version='0.2',
+           description='Acadbot for JUIT to fetch attendance, timetable etc...',
+           author='dev@ver',
+           author_email='eternal.blizzard23@gmail.com, \
+                         kashmiri.devesh@gmail.com',
+           scripts = [
+                'acadbot.py'
+           ]
+)
