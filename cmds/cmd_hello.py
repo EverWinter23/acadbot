d99ced37600b59a79e68398394a305f7a51547d4
Ever Winter <eternal.blizzard23@gmail.com>
2018-04-19 02:32:27 +0530

    cmd_hello.py: Display usage help with the greeting

--- cmds/cmd_hello.py
+++ cmds/cmd_hello.py
@@ -1,20 +1,27 @@
+'''
+19th april thursday 2018
+'''
 from pathlib import Path
 from cmds.helper import Helper
+from tabulate import tabulate
 
 CONFIG_FILE = 'config.txt'
+helper = Helper()
 
 def cmd_hello():
     file = Path(CONFIG_FILE)
     if not file.exists():
         print('acadbot: Hello user. It appears you have not configured acadbot yet..')
         print('acadbot: Please run the following commands first...')
-        Helper().help_config()
+        helper.help_config()
         print('acadbot: Exiting...')
         exit()
     else:
         with open(file, 'r') as config_file:
             username = config_file.readline().rstrip('\n')
-            user = username.strip('username:')
-        print('acadbot: Hello ' + str(user) + '. Acadbot is happy to help you with your WebKiosk needs...')
+        print('acadbot: Hello ' + username + '!')
+        print('acadbot: I am happy to help you with your WebKiosk needs...')
+        print('')
+        helper.help_helper()
         print('')
         exit()
